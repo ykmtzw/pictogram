3177ce4d6364edae370a58ec3aac3ed3137e30d1
チヴァース 陽子 <yokochivers@chivuasu-no-MacBook-Air.local>
2018-08-25 15:33:40 +0900

    second commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require bootstrap-sprockets
+//= require_tree.
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      redirect_to root_path, success:'登録が完了しました'
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
+  end
+end
